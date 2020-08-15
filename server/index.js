08ec1f937995a296d608d959275d86c30df01bfa
Rajvardhan <rajthakur3619@gmail.com>
2020-08-16 00:29:57 +0530

    implementing join component

--- server/index.js
+++ server/index.js
@@ -4,9 +4,21 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express()
 const server = http.createServer(app);
 const io = scoketio(server);
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection');
+
+    socket.io('disconnect', () => {
+        console.log('User has Left the chat!!!!')
+    })
+})
+
+app.use(router);
 
 server.listen(PORT, () => {
     console.log(`Server has started at ${PORT}`);
