96a3a9f6ed6f32b4847d9855b945f1b3f7a70fc1
Rajvardhan <rajthakur3619@gmail.com>
2020-08-16 22:30:29 +0530

    sendMessage emmitter implementd

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users')
+
 const router = require('./router');
 
 const app = express()
@@ -11,10 +13,30 @@ const server = http.createServer(app);
 const io = scoketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection');
 
     socket.on('join', ({name, room}, callback) => {
-        console.log(name, room)
+        const { error, user} = addUser({id: socket.id, name, room});
+
+        if(error) return callback(error)
+
+        socket.emit('message', {user: 'admin', text:`${user.name}, welcome to the ${user.room}`})
+
+        socket.broadcast.to(user.room).emit('message', {user:'admin', text:`${user.name}, has joined!!!`})
+
+        socket.join(user.room)
+
+        callback();
+    })
+
+
+    socket.on('sendMessage', (message, callback)=>{
+        const user = getUser(socket.id);
+
+        console.log(user)
+
+        io.to(user.room).emit('message', { user:user.name, text: message})
+
+        callback();
     })
 
     socket.on('disconnect', () => {
